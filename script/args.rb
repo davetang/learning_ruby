4dcb6879648b4e889dce92ac59088844ae45c3f5
Dave Tang <davetingpongtang@gmail.com>
2023-03-14 10:29:07 +0900

    Positional and optional arguments

--- script/args.rb
+++ script/args.rb
@@ -1,31 +1,49 @@
 #!/usr/bin/env ruby
 #
 # https://docs.ruby-lang.org/en/master/optparse/tutorial_rdoc.html
+# https://docs.ruby-lang.org/en/2.3.0/OptionParser.html
 #
 
 # require OptionParser
 require 'optparse'
 
+usage = -<<EOF
+Usage: #{$0} [-h] [-v] [-t INT] read1 read2
+
+positional arguments:
+    read1                            first read pair
+    read2                            second read pair
+
+optional arguments:
+    -h, --help                       display usage
+EOF
+
 # create an OptionParser object
 option_parser = OptionParser.new do |opts|
-  opts.on('-x', '--xxx', 'Short and long, no argument')
-  opts.on('-y YYY', '--yyy', 'Short and long, required argument')
-  opts.on('-z [ZZZ]', '--zzz', 'Short and long, optional argument')
+  opts.banner = usage
+  opts.on('-v', '--verbose', 'verbose mode')
+  opts.on('-t [8]', '--threads [8]', "number of threads (default: 8)")
 end
 
-# check for missing options
-options = {}
+# set default
+options = { threads: 8 }
 # ARGV will be modified destructively
 option_parser.parse!(into: options)
-required_options = [:xxx, :yyy]
-missing_options = required_options - options.keys
-unless missing_options.empty?
-  fail "Missing required options: #{missing_options}"
+
+if ARGV.length != 2
+  puts option_parser
+  exit
+end
+
+fastq1 = ARGV[0]
+fastq2 = ARGV[1]
+
+if options[:verbose]
+  puts "Verbose mode"
 end
 
 # access input options
-puts "yyy is #{options[:yyy]}"
-puts "xxx is #{options[:xxx]}"
-if options[:zzz]
-  puts "zzz is #{options[:zzz]}"
-end
+puts "Read 1: #{fastq1}"
+puts "Read 2: #{fastq2}"
+puts "Using #{options[:threads]} threads"
+exit
